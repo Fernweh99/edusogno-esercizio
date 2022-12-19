b8da15dac3e1c157fdcaa90848dc80511f7283a1
Fernweh99 <daniele.dimento@gmail.com>
2022-12-19 13:21:26 +0100

    redirect to login from index

--- index.php
+++ index.php
@@ -5,11 +5,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FontAw -->
+    <script src="https://kit.fontawesome.com/7ff5ce46be.js" crossorigin="anonymous"></script>
+    <!-- Style -->
+    <link rel="stylesheet" href="../assets/styles/style.css">
     <title>Edusogno</title>
 </head>
 
 <body>
-
+ <?php
+    header("Location: pages/login.php");
+    exit();
+ ?>
 </body>
 
 </html>
